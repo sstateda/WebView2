4044c34402f8deaf6c73331c74e616b5e86560d0
ss_tateda <ss_tateda@shift.gr.jp>
2016-05-16 11:59:57 +0900

    first commit
    test

---
+++ Assets/myWebView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myWebView : MonoBehaviour
+{
+    private string url = "http://google.co.jp";    // WebViewに表示するURL
+    WebViewObject webViewObject;
+
+    void Start()
+    {
+        webViewObject = GetComponent<WebViewObject>();
+        webViewObject.Init();
+        webViewObject.LoadURL(url);
+        webViewObject.SetVisibility(true);
+        webViewObject.SetMargins(0, 0, 0, 200);    // マージン(単位px) 左、上、右、下
+    }
+}
